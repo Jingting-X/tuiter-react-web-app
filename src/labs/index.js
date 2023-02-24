475e0fe4dd6d3840fd41b15748447b1ccf3d55c8
Jingting <xingjingting1217@gmail.com>
2023-02-24 15:50:21 -0800

    A6 React v0.1: part 1

---
+++ src/labs/index.js
@@ -0,0 +1,13 @@
+import Assignment6 from "./a6";
+import Nav from "../nav.js";
+
+function Labs() {
+    return(
+        <div>
+            {/*<h1>Assignment 6</h1>*/}
+            <Nav/>
+            <Assignment6/>
+        </div>
+    );
+}
+export default Labs;
---
+++ src/tuiter/index.js
@@ -0,0 +1,15 @@
+import {Link} from "react-router-dom";
+import Nav from "../nav.js";
+
+function Tuiter() {
+    return(
+        <div>
+            {/*<Link to="/">Lab</Link> |*/}
+            {/*<Link to="/hello">Hello</Link> |*/}
+            {/*<Link to="/tuiter">Tuiter</Link>*/}
+            <Nav/>
+            <h1>Tuiter</h1>
+        </div>
+    );
+}
+export default Tuiter
